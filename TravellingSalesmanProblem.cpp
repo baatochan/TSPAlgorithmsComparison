1a9847279e6df65e40ae902b7c5f540930d18fd2
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-11-03 21:47:49 +0100

    Add method for generating random tsp data

--- TravellingSalesmanProblem.cpp
+++ TravellingSalesmanProblem.cpp
@@ -3,18 +3,44 @@
 //
 
 #include <stdexcept>
+#include <random>
 #include "TravellingSalesmanProblem.h"
 
-TravellingSalesmanProblem::TravellingSalesmanProblem() {
-
-}
+TravellingSalesmanProblem::TravellingSalesmanProblem() {}
 
 void TravellingSalesmanProblem::loadDataFromFile(std::string path) {
 	throw std::runtime_error("Not yet implemented!");
 }
 
 void TravellingSalesmanProblem::generateRandomData(int numberOfCities, int range) {
-	throw std::runtime_error("Not yet implemented!");
+	if (numberOfCities < 0) {
+		throw std::runtime_error("Liczba miast nie moze byc ujemna!");
+	}
+	if (numberOfCities == 0) {
+		TSPData.clear();
+		TSPData.resize(0);
+		return;
+	}
+	if (range < 1) {
+		throw std::runtime_error("Gorna granica zakresu musi byc dodatnia!");
+	}
+
+	std::random_device seed;
+	std::mt19937 randomGenerator(seed());
+	std::uniform_int_distribution<> rangeTransformer(1, range);
+
+	TSPData.clear();
+	TSPData.resize(numberOfCities);
+
+	for (auto &row : TSPData) {
+		for (int j = 0; j < numberOfCities; ++j) {
+			row.push_back(rangeTransformer(randomGenerator));
+		}
+	}
+
+	for (int i = 0; i < TSPData.size(); ++i) {
+		TSPData[i][i] = -1;
+	}
 }
 
 std::string TravellingSalesmanProblem::printData() {
