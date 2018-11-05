d2ebcc489a98a77b5dae40d09418e289f942aec3
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-11-06 00:09:50 +0100

    Add prepare to run method
    it's exactly the same as bf one, it should be refactored... TTvTT

--- BranchAndBound.cpp
+++ BranchAndBound.cpp
@@ -2,14 +2,32 @@
 // Created by barto on 03.11.18.
 //
 
+#include <cmath>
 #include "BranchAndBound.h"
 
 BranchAndBound::BranchAndBound(std::shared_ptr<TravellingSalesmanProblem> TSP) : Algorithm(std::move(TSP)) {}
 
 std::string BranchAndBound::prepareToRun() {
-	// TODO: Not yet implemented!
-	throw std::runtime_error("Not yet implemented!");
-	return std::__cxx11::string();
+	numberOfCities = TSP->getNumberOfCities();
+	if (numberOfCities < 2) {
+		throw std::runtime_error("Macierz miast jest pusta, badz zawiera tylko jedno miasto!");
+	}
+
+	std::string output;
+
+	output += "Ilosc permitacji: ";
+	output += std::to_string(static_cast<unsigned long>(round(std::tgamma(numberOfCities))));
+	/**
+	 * number of permutations -> (n-1)!; gamma(n) = (n-1)!
+	 *
+	 * gets the number of cities, calculates gamma function (explained above), result is stored as a double,
+	 * rounds to int, the result of rounding is still the double, casts into int and coverts that int to string :D
+	 * don't kill me, my future self xD
+	 *
+	 */
+	output += "\n";
+
+	return output;
 }
 
 std::string BranchAndBound::run() {
