8aaf71077d5a438c7739d8599d817cfa28856617
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-11-07 01:02:26 +0100

    Add test backbone + file support

---
+++ Test.cpp
@@ -0,0 +1,107 @@
+//
+// Created by barto on 07.11.18.
+//
+
+#include "Test.h"
+
+Test::Test() {
+	openFile();
+}
+
+Test::~Test() {
+	outputFile.close();
+}
+
+std::string Test::test1() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test2() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test3() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test4() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test5() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test6() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test7() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test8() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::test9() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string Test::getTestName(char test) {
+	switch (test) {
+		case '1':
+			return "Not yet implemented!";
+
+		case '2':
+			return "Not yet implemented!";
+
+		case '3':
+			return "Not yet implemented!";
+
+		case '4':
+			return "Not yet implemented!";
+
+		case '5':
+			return "Not yet implemented!";
+
+		case '6':
+			return "Not yet implemented!";
+
+		case '7':
+			return "Not yet implemented!";
+
+		case '8':
+			return "Not yet implemented!";
+
+		case '9':
+			return "Not yet implemented!";
+
+		default:
+			return "";
+	}
+}
+
+void Test::openFile() {
+	std::string path = "..\\wyniki\\";
+	path += "test-";
+	path += std::to_string(time(0));
+	path += ".txt";
+
+	outputFile.open(path, std::fstream::out);
+
+	if (!outputFile.is_open()) {
+		throw std::runtime_error("Nie mozna otworzyc pliku (sprawdz czy istnieje katalog wyniki)!");
+	}
+
+	outputFile.setf(std::ios::fixed);
+}
---
+++ Test.h
@@ -0,0 +1,37 @@
+//
+// Created by barto on 07.11.18.
+//
+
+#ifndef TSPALGORITHMSCOMPARISON_TEST_H
+#define TSPALGORITHMSCOMPARISON_TEST_H
+
+
+#include <fstream>
+
+class Test {
+public:
+	Test();
+
+	~Test();
+
+	std::string test1();
+	std::string test2();
+	std::string test3();
+	std::string test4();
+	std::string test5();
+	std::string test6();
+	std::string test7();
+	std::string test8();
+	std::string test9();
+
+	std::string getTestName(char test);
+
+private:
+
+	std::fstream outputFile;
+
+	void openFile();
+};
+
+
+#endif //TSPALGORITHMSCOMPARISON_TEST_H
