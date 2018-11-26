8a537a45ff301ba9f2f9189784544e9411ccedd3
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-11-27 00:59:00 +0100

    Add needed vars for TS

--- TabuSearch.cpp
+++ TabuSearch.cpp
@@ -17,3 +17,14 @@ std::string TabuSearch::run() {
 	throw std::runtime_error("Not yet implemented!");
 	return std::__cxx11::string();
 }
+
+void TabuSearch::prepareToRun() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+}
+
+std::string TabuSearch::generateOutput() {
+	// TODO: Not yet implemented!
+	throw std::runtime_error("Not yet implemented!");
+	return std::__cxx11::string();
+}
--- TabuSearch.h
+++ TabuSearch.h
@@ -17,7 +17,21 @@ public:
 	std::string run() override;
 
 private:
+	void prepareToRun() override;
 
+	std::string generateOutput() override;
+
+	// city 1, city 2, cadency; blocks exchanging the pair of cities
+	std::vector<std::tuple<int, int, int>> tabuList;
+
+	// TS parameters TODO: some kind of UI to change it
+	int cadency;
+	int tabuSize;
+	double endTime;
+	bool aspiration;
+	int iterationsToChangeNeighborhood;
+
+	int iterationWithoutChangingNeighborhood;
 };
 
 
